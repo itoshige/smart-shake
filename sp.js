5462a70128553898454d880b0920537f3c9b6013
itoshige <itoshige1017@gmail.com>
2015-12-01 22:52:45 +0900

    modify

--- sp.js
+++ sp.js
@@ -16,7 +16,7 @@ function furu(e) {
 	var z = e.accelerationIncludingGravity.z; // Z•ûŒü‚Ì‰Á‘¬“x
 
 	// ‰Á‘¬“x‚ªˆê’èˆÈã‚Ì‚Æ‚«
-	if (Math.abs(x) > 200 || Math.abs(y) > 200 || Math.abs(z) > 50) {
+	if (Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15) {
 	// ‚±‚±‚ÉU‚Á‚Ä‚¢‚é‚Æ‚«‚Ì“®ì‚ğ“ü‚ê‚é
 		alert("shake!");
 //		count++;
