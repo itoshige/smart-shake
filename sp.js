4b93d9ec4bdd2bfab5a6cbc54e1cf8105da75af4
itoshige <itoshige1017@gmail.com>
2015-12-01 23:23:54 +0900

    m

--- sp.js
+++ sp.js
@@ -14,7 +14,7 @@ function furu(e) {
 	var z = e.accelerationIncludingGravity.z; // Z•ûŒü‚Ì‰Á‘¬“x
 
 	// ‰Á‘¬“x‚ªˆê’èˆÈã‚Ì‚Æ‚«
-	if (Math.abs(x) > 20 || Math.abs(y) > 20 || Math.abs(z) > 20) {
+	if (Math.abs(x) > 30 || Math.abs(y) > 30 || Math.abs(z) > 20) {
 	// ‚±‚±‚ÉU‚Á‚Ä‚¢‚é‚Æ‚«‚Ì“®ì‚ğ“ü‚ê‚é
 		alert("shake!");
 //		count++;
