35aa4236e058f8917b045395c08059cc6cf279de
itoshige <itoshige1017@gmail.com>
2015-12-02 16:09:32 +0900

    m

--- pc.js
+++ pc.js
@@ -5,8 +5,7 @@ var countbox = document.getElementById('count');
 ds.on('send', countview);
 
 function countview(sent){
-	alert("test");
-//	if(sent.value.data.count >= 2){
+/	if(sent.value.data.count >= 2){
 //		var oldelement = document.getElementById(sent.value.data.name);
 //		oldelement.parentNode.removeChild(oldelement);
 //	}
