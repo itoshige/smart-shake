08f3c18adc2bd10f0bc3f30cb29d8ecde4a7a7a2
itoshige <itoshige1017@gmail.com>
2015-12-02 21:33:45 +0900

    m

--- sp.js
+++ sp.js
@@ -5,7 +5,6 @@ var count = 0;
 var countbox = document.getElementById('count');
 var nameform = document.getElementById('nameform');
 
-
 var shakecheck = function(name) {
   $(this).gShake(function() {
     count++;
@@ -23,8 +22,6 @@ function namefunc(){
     shakecheck(name);
 } 
 
-
-
 //â¸çsÇ≈submitÇ≥ÇπÇ»Ç¢ÇÊÇ§Ç…Ç∑ÇÈ
 function submitStop(e){
     if (!e) var e = window.event;
@@ -32,3 +29,44 @@ function submitStop(e){
     if(e.keyCode == 13)
         return false;
 }
+
+
+var codes = {
+  48: 0,
+  49: 1,
+  50: 2,
+  51: 3,
+  52: 4,
+  53: 5,
+  54: 6,
+  55: 7,
+  56: 8,
+  57: 9
+};
+
+var keydownHelper = function (e) {
+  e.preventDefault();
+  countbox.removeEventListener("input", inputHelper); 
+
+  var val = countbox.value;
+
+  // Delete
+  if (e.keyCode === 8 && val.length) {
+    countbox.value = val.slice(0, val.length - 1);
+    return;
+  }
+
+  // If not a number, do nada
+  if (typeof codes[e.keyCode] === "undefined") { return; }
+
+  val += codes[e.keyCode];
+  countbox.value = val;
+};
+
+var inputHelper = function (e) {
+  e.preventDefault();
+  window.removeEventListener("keydown", keydownHelper);
+};
+
+countbox.addEventListener("input", inputHelper);
+window.addEventListener("keydown", keydownHelper); 
